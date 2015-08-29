b2290ec94708720ab60da3a9ae3417a7aab8da95
agpadgett <agpadgett89@gmail.com>
2015-08-29 12:55:47 -0700

    added database

---
+++ hairsalondatabase.sql
@@ -0,0 +1,182 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: clients; Type: TABLE; Schema: public; Owner: Andrea; Tablespace:
+--
+
+CREATE TABLE clients (
+    id integer NOT NULL,
+    name character varying,
+    stylist_id integer
+);
+
+
+ALTER TABLE clients OWNER TO "Andrea";
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE; Schema: public; Owner: Andrea
+--
+
+CREATE SEQUENCE clients_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE clients_id_seq OWNER TO "Andrea";
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Andrea
+--
+
+ALTER SEQUENCE clients_id_seq OWNED BY clients.id;
+
+
+--
+-- Name: stylists; Type: TABLE; Schema: public; Owner: Andrea; Tablespace:
+--
+
+CREATE TABLE stylists (
+    id integer NOT NULL,
+    stylist_name character varying
+);
+
+
+ALTER TABLE stylists OWNER TO "Andrea";
+
+--
+-- Name: stylists_id_seq; Type: SEQUENCE; Schema: public; Owner: Andrea
+--
+
+CREATE SEQUENCE stylists_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE stylists_id_seq OWNER TO "Andrea";
+
+--
+-- Name: stylists_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Andrea
+--
+
+ALTER SEQUENCE stylists_id_seq OWNED BY stylists.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Andrea
+--
+
+ALTER TABLE ONLY clients ALTER COLUMN id SET DEFAULT nextval('clients_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: Andrea
+--
+
+ALTER TABLE ONLY stylists ALTER COLUMN id SET DEFAULT nextval('stylists_id_seq'::regclass);
+
+
+--
+-- Data for Name: clients; Type: TABLE DATA; Schema: public; Owner: Andrea
+--
+
+COPY clients (id, name, stylist_id) FROM stdin;
+1	bob	0
+2	bob	0
+3	bob	5
+4	sal	5
+5	elle	6
+6	suzy	6
+14	jaime	11
+15	jon	14
+17	stan	15
+\.
+
+
+--
+-- Name: clients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Andrea
+--
+
+SELECT pg_catalog.setval('clients_id_seq', 19, true);
+
+
+--
+-- Data for Name: stylists; Type: TABLE DATA; Schema: public; Owner: Andrea
+--
+
+COPY stylists (id, stylist_name) FROM stdin;
+13	bob
+15	jon
+\.
+
+
+--
+-- Name: stylists_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Andrea
+--
+
+SELECT pg_catalog.setval('stylists_id_seq', 15, true);
+
+
+--
+-- Name: clients_pkey; Type: CONSTRAINT; Schema: public; Owner: Andrea; Tablespace:
+--
+
+ALTER TABLE ONLY clients
+    ADD CONSTRAINT clients_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: stylists_pkey; Type: CONSTRAINT; Schema: public; Owner: Andrea; Tablespace:
+--
+
+ALTER TABLE ONLY stylists
+    ADD CONSTRAINT stylists_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
